ebabf0c6dcbc31a90df70d402ca5b8ada20db7e8
Ivan Asonov <asonovivan88@gmail.com>
2020-11-03 07:34:54 +0100

    utility node class

---
+++ src/main/java/com/a3nv/util/Node.java
@@ -0,0 +1,56 @@
+package com.a3nv.util;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ */
+public class Node {
+
+    private int val;
+
+    private Node next;
+
+    public Node() {
+    }
+
+    public Node(int val) {
+        this.val = val;
+    }
+
+    public Node(int val, Node next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+
+    @Override
+    public String toString() {
+        Node current = this;
+        StringBuilder sb = new StringBuilder();
+        while (current != null) {
+            sb.append(current.getVal());
+            if (current.getNext() != null) {
+                sb.append(" -> ");
+            }
+            current = current.getNext();
+        }
+        return sb.toString();
+    }
+}

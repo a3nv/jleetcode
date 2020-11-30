b41b164f5062f5456468daf80b21e06a41217ba3
Ivan Asonov <asonovivan88@gmail.com>
2020-11-30 13:14:33 +0100

    1306. Jump Game III java doc
    https://leetcode.com/problems/jump-game-iii/

--- src/main/java/com/a3nv/solutions/M1306JumpGame3.java
+++ src/main/java/com/a3nv/solutions/M1306JumpGame3.java
@@ -5,6 +5,20 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Set;
 
+/**
+ * Given an array of non-negative integers arr, you are initially positioned at start index of the array. When you are at index i, you can jump to i + arr[i] or i - arr[i], check if you can reach to any index with value 0.
+ *
+ * Notice that you can not jump outside of the array at any time.
+ *
+ * Input: arr = [4,2,3,0,3,1,2], start = 5
+ * Output: true
+ * Explanation:
+ * All possible ways to reach at index 3 with value 0 are:
+ * index 5 -> index 4 -> index 1 -> index 3
+ * index 5 -> index 6 -> index 4 -> index 1 -> index 3
+ *
+ * <a href="https://leetcode.com/problems/jump-game-iii/">Jump game 3</a>
+ */
 public class M1306JumpGame3 {
 
     public static boolean canReach(int[] arr, int start) {

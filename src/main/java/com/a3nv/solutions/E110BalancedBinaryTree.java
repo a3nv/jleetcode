84af0e10603c0e00b29bb7a96a6b277b6bbc4c79
Ivan Asonov <asonovivan88@gmail.com>
2020-12-22 19:55:40 +0100

    110. Balanced Binary Tree
    solution;
    
    https://leetcode.com/problems/balanced-binary-tree/

--- src/main/java/com/a3nv/solutions/E110BalancedBinaryTree.java
+++ src/main/java/com/a3nv/solutions/E110BalancedBinaryTree.java
@@ -42,8 +42,33 @@ import com.a3nv.util.TreeNode;
  */
 public class E110BalancedBinaryTree {
 
-    public boolean isBalanced(TreeNode root) {
+    public static boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        int lh = getHeight(root.getLeft());
+        int rh = getHeight(root.getRight());
 
-        return false;
+        return Math.abs(lh - rh) <= 1 && isBalanced(root.getLeft()) && isBalanced(root.getRight());
+    }
+
+    private static int getHeight(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return 1 + Math.max(getHeight(node.getLeft()), getHeight(node.getRight()));
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15),
+                new TreeNode(7)));
+        boolean result = isBalanced(root);
+        System.out.println("Tree is balanced: " + result);
+
+
+        root = new TreeNode(1, new TreeNode(2, new TreeNode(3, new TreeNode(4), new TreeNode(4)), new TreeNode(3)),
+                new TreeNode(2));
+        result = isBalanced(root);
+        System.out.println("Tree is balanced: " + result);
     }
 }
